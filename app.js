144ad515fddc3eeaf6992d936b54deb45187a97b
andersons1218 <andersons1218@gmail.com>
2022-05-23 16:07:59 -0400

    Build-A-Man drafts

--- app.js
+++ app.js
@@ -2,25 +2,22 @@
 let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 let categoryTitle = [ 'City', 'Drink']
-let words = ['Los Angeles', 'London','New York','Tokyo','Seoul',
-], [ 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Mimosa']
-let hints = [" Most populated city in America","Everyone loves a queen","Blank Food", "Get amazing sushi from here","City of Angles" ], ["A popular Mexican drink","A drink made famous by tv show", "A Drink originated in cuba","a Sunday funday drink"]
+let words = [['New York', 'London','Seoul', 'Tokyo','Los Angeles'
+], [ 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Mimosa']]
+let hints = [[" Most populated city in America","Everyone loves a queen","Blank Food", "Get amazing sushi from here","City of Angles" ], ["This drink can be shaken not stirred", "A popular Mexican drink","A drink made famous by tv show for single women", "A Drink originated in cuba","a Sunday funday drink"]]
 
 
-let categoryChoice =
-
-//Picks Category
-let chooseCategory = function () {
-    if ( === categoryTitle )
-}
 
 //picks Word
 
 //click hint and hints show up 
 
-hint.onClick = fuction () {
-    category = category.indexOf(category);
-    hintArray = chosen.indexOf
+const letters = document.querySelectorAll('.letters')
+for (const letter of letters){
+    if (letter)
+    letter.addEventListener('click', function(){
+        return letter.innerText
+    })
 }
 //play again function 
 
