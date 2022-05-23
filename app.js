b380fbbcbdc8e19ea56368b9251dd9e9b0ffbbac
andersons1218 <andersons1218@gmail.com>
2022-05-23 15:34:02 -0400

    Build-A-Man drafts

--- app.js
+++ app.js
@@ -2,20 +2,19 @@
 let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 let categoryTitle = [ 'City', 'Drink']
-let cityList = ['Los Angeles', 'London','New York','Tokyo','Seoul',
-]
-let drinksList = [ 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Mimosa']
+let words = ['Los Angeles', 'London','New York','Tokyo','Seoul',
+], [ 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Mimosa']
 let hints = [" Most populated city in America","Everyone loves a queen","Blank Food", "Get amazing sushi from here","City of Angles" ], ["A popular Mexican drink","A drink made famous by tv show", "A Drink originated in cuba","a Sunday funday drink"]
+
 
 let categoryChoice =
 
-
-/
 //Picks Category
 let chooseCategory = function () {
     if ( === categoryTitle )
 }
 
+//picks Word
 
 //click hint and hints show up 
 
@@ -23,3 +22,5 @@ hint.onClick = fuction () {
     category = category.indexOf(category);
     hintArray = chosen.indexOf
 }
+//play again function 
+
