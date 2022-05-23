cbcb91157eea7089aaf5fb64a9b347eab1f1e921
andersons1218 <andersons1218@gmail.com>
2022-05-23 14:32:11 -0400

    Build-A-Man drafts

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 let categoryTitle = [ 'City', 'Drink']
-let cityList = [ 'Boston','Los Angeles', 'London', 'Dubia', 'Miami','New York', 'Sydney', 'Chicago', 'Tokyo','Seoul',
+let cityList = ['Los Angeles', 'London','New York','Tokyo','Seoul',
 ]
-let drinksList = ['vodka soda','Side Car', 'Old Fashion', 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Moscow Mule', 'Paloma']
+let drinksList = [ 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Mimosa']
+let hints = [" Most populated city in America","Everyone loves a queen","Blank Food", "Get amazing sushi from here","City of Angles" ], ["A popular Mexican drink","A drink made famous by tv show", "A Drink originated in cuba","a Sunday funday drink"]
+
+let categoryChoice =
+
+
+/
+//Picks Category
+let chooseCategory = function () {
+    if ( === categoryTitle )
+}
+
+
+//click hint and hints show up 
+
+hint.onClick = fuction () {
+    category = category.indexOf(category);
+    hintArray = chosen.indexOf
+}
