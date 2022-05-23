85b43893919b996609f83856a0a6e5d9086d5361
andersons1218 <andersons1218@gmail.com>
2022-05-23 10:53:55 -0400

    Build-A-Man drafts

---
+++ app.js
@@ -0,0 +1,11 @@
+//different variables
+let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let getHint;
+let word;
+let guess;
+let guesses;
+let lives;
+let counter;
+let space;
+
+//
