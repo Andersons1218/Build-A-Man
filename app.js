e04deeb13ead2a87465519e5777646aeb62cb6a0
andersons1218 <andersons1218@gmail.com>
2022-05-23 11:32:47 -0400

    Build-A-Man Drafts

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 //different variables
 let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let getHint;
-let word;
-let guess;
-let guesses;
-let lives;
-let counter;
-let space;
 
-//
+let categoryTitle = [ 'City', 'Drink']
+let cityList = [ 'Boston','Los Angeles', 'London', 'Dubia', 'Miami','New York', 'Sydney', 'Chicago', 'Tokyo','Seoul',
+]
+let drinksList = ['vodka soda','Side Car', 'Old Fashion', 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Moscow Mule', 'Paloma']
