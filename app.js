c652e155e7e7de3a1dbe5580f93828f304d78585
andersons1218 <andersons1218@gmail.com>
2022-05-24 14:11:51 -0400

    Build-A-Man drafts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //different variables
 let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-let words = ['New York', 'London','Seoul', 'Tokyo','Los Angeles', 'Martini', 'Mojito', 'Margarita', 'Cosmopolitan', 'Mimosa']
+let words = ["pizza", "cheeseburger", "fries", "pancake"]
 
 
 //picks Word
@@ -15,20 +15,20 @@ for (let i = 0; i < words.length; i++){
     answerArray[i] = "_";
 }
 let remainingLetters = words.length;
+
 //Game Loop
 while(remainingLetters > 0) {
-    alert(answerArray.join(" "));
     let guess = promt("Guess a letter");
-}if (guess === null){
-    break;
-}else if (guess.length !==1){
-    alert("please enter letter");
+}if (guess === null) {
+    break ;
+}else if (guess.length !== 1) {
 }else{
     for (let x = 0; x < words.length; x++){
         if (word[x]=== guess){
             answerArray[x] = guess;
-            remainingLetter--;
+            remainingLetter - -;
         }
     }
 }
+
 
