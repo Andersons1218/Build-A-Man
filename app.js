e112446f469dd4cef12b05b865aa21ce7ec06163
andersons1218 <andersons1218@gmail.com>
2022-05-25 12:03:54 -0400

    Build-A-Man js  refactored

--- app.js
+++ app.js
@@ -1,47 +1,32 @@
 //different variables
-let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const playAgain = document.querySelector('.playagain')
+const reset = document.querySelector('.reset')
+const word = document.querySelector('.word')
+const letterbutton = document.querySelector('.letterbutton')
 
 let words = ["pizza", "cheeseburger", "fries", "pancake"]
 let theWord= null;
+let letter;
 
 
-function main () {
     
 
-//picks Word
+// //picks Word
 function randomWord (){
     theWord = words[Math.floor(Math.random()*words.length)];
     
 }
-randomWord()
-//set up answer array
-let answerArray = [];
-for (let i = 0; i < theWord.length; i++){
-    answerArray[i] = "_";
-}
-let remainingLetters = theWord.length;
+randomWord();
 
+// 
+let select_word;
 
-//Game Loop
-while(remainingLetters > 0) {
-}if (guess === null) {
-}else if (guess.length !== 1) {
-}else{
-    for (let x = 0; x < words.length; x++){
-        if (words[x]=== guess){
-            answerArray[x] = guess;
-            remainingLetters--;
-        }
+const main = function (state) {
+  word.innerHTML = '';
+  if (state === 'start') {
+    // putting all letters into html
+    for (const i of 'abcdefghijklmnopqrstuvwxyz') {
+      const html = `<button class="alpha">${i.toUpperCase()}</button>`;
+      letter.insertAdjacentHTML('beforeend', html);
     }
-}
-}
-main();
-
-for (let z = 0; z < alphabet.length; i++){
-// const buttons =  document.getElementsByClassName('.buttons');
-const butttons = document.createElement("buttons")
-butttons.setAttribute('id', alphabet[z])
-}
-    
-
-
+ 
