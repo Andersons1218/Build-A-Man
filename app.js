db8b87b1c1df0f594d80806c715f6f16e6050a1f
andersons1218 <andersons1218@gmail.com>
2022-05-24 20:58:33 -0400

    Build-A-Man drafts

--- app.js
+++ app.js
@@ -2,35 +2,46 @@
 let alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 let words = ["pizza", "cheeseburger", "fries", "pancake"]
+let theWord= null;
 
+
+function main () {
+    
 
 //picks Word
 function randomWord (){
-    let theWord = words[Math.floor(Math.random()*words.length)];
-    alert(theWord)
+    theWord = words[Math.floor(Math.random()*words.length)];
+    
 }
 randomWord()
 //set up answer array
 let answerArray = [];
-for (let i = 0; i < words.length; i++){
+for (let i = 0; i < theWord.length; i++){
     answerArray[i] = "_";
 }
-let remainingLetters = words.length;
+let remainingLetters = theWord.length;
+
 
 //Game Loop
 while(remainingLetters > 0) {
-    let guess = promt("Guess a letter");
 }if (guess === null) {
-    break ;
 }else if (guess.length !== 1) {
 }else{
     for (let x = 0; x < words.length; x++){
-        if (word[x]=== guess){
+        if (words[x]=== guess){
             answerArray[x] = guess;
-            remainingLetter - -;
+            remainingLetters--;
         }
     }
 }
+}
+main();
 
+for (let z = 0; z < alphabet.length; i++){
+// const buttons =  document.getElementsByClassName('.buttons');
+const butttons = document.createElement("buttons")
+butttons.setAttribute('id', alphabet[z])
+}
+    
 
 
