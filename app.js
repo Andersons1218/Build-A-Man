d60c86cb23c5540fd58dd7679ad97ad6ac3ad58e
andersons1218 <andersons1218@gmail.com>
2022-05-26 12:32:49 -0400

    Build-a-man refactored losing condition

--- app.js
+++ app.js
@@ -1,110 +1,91 @@
 //different variables
 let selectWord;
+console.log(selectWord);
+const winningMessage = document.querySelector('.winningmesg')
+const gameovermessage = document.querySelector('.gameover')
+const wordContel = document.querySelector(".wordCont");
+const letterButton = document.querySelector(".letterbuttons");
+const numberOfTries = document.querySelector(".numberOfTries");
+const guesses = document.querySelector(".guesses");
+let words = ["pizza", "chips", "fries", "pancake"];
+let tries = 5;
+let theWord;
 
-const spaces = document.querySelector('.spaces')
-const letterButton = document.querySelector('.letterbuttons')
-const numberOfTries = document.querySelector('.numberOfTries');
-const guesses = document.querySelector('.guesses')
-let words = ["pizza", "cheeseburger", "fries", "pancake"]
-let tries;
-
-    
-
- //page refreshes picks random Word
-function randomWord (){
-   return theWord = words[Math.floor(Math.random()*words.length)];
-  
+//page refreshes picks random Word
+function randomWord() {
+  return (theWord = words[Math.floor(Math.random() * words.length)]);
 }
 randomWord();
-
-
+console.log(theWord.length);
 
 //adding letters to HTML
 const initiate = function (game) {
-  spaces.innerHTML = ' ';
-  if (game === 'start') {
-    for (const i of 'abcdefghijklmnopqrstuvwxyz') {
+  numberOfTries.textContent = tries;
+  // spaces.innerHTML = ' ';
+  if (game === "start") {
+    for (const i of "abcdefghijklmnopqrstuvwxyz") {
       const html = `<button class="alphabet">${i.toUpperCase()}</button>`;
-      letterButton.insertAdjacentHTML('beforeend', html);
+      letterButton.insertAdjacentHTML("beforeend", html);
     }
-  } else if (game === 'reset') {
-    lettersButtons.forEach(button => {
-      button.classList.remove('disabled');
+  } else if (game === "reset") {
+    lettersButtons.forEach((button) => {
+      button.classList.remove("disabled");
     });
   }
-  selectWord = randomWord(spaces);
-  tries = 5;
+  selectWord = randomWord(wordContel);
+  console.log(selectWord);
 
- letterButtons = document.querySelectorAll('.alphabet');
- numberOfTries.textContent = tries;
-
- // creating lines and selecting the words 
- for (let i = 0; i < theWord.length; i++) {
-   const html = `<p class="spaces">_</p>`;
-   spaces.insertAdjacentHTML('beforeend', html);
- }
-};
-
-initiate('start');
-
-
-// Amount of tries go down and alerts loser
-const triesGoDown = function () {
- tries--;
- numberOfTries.textContent = tries;
- if (tries === 0) {
-   alert('lost');
- }
-};
-// check if there is a complete word
-const checkWord = function () {
- let guessedWord = true;
- for (let i = 0; i < spaces.children.length; i++) {
-   if (spaces.children[i].textContent === '_') {
-     guessedWord = false;
-   }
- }
- return guessedWord;
-};
-//checks to see if letter fits into word
-const getindexes = function (letter) {
- let indexes = [];
- [spaces].forEach((y, i) => {
-   if (y === letter) {
-     const index = i;
-     indexes.push(index);
-     
-   }
- });
-
- return indexes;
-};
-
-
-// winning and losing conditions
-const letterPress = function () {
-    const letter = this.textContent.toLowerCase();
-  console.log(selectWord)
-    if (selectWord.includes(letter)) {
-      const indexes_list = getindexes(letter);
-      indexes_list.forEach((x, i) => {
-        spaces.children[x].textContent = this.textContent;
-      });
-      if (checkWord()) alert('you win');
-    } else {
-      triesGoDown();
+  creatWordCont = function () {
+    for (let z = 0; z < selectWord.length; z++) {
+      //console.log(selectWord[z])
+      let letterCont = document.createElement("div");
+      letterCont.classList.add(selectWord[z]);
+      letterCont.classList.add("letter");
+      // console.log(letterCont)
+      wordContel.appendChild(letterCont);
     }
-
-       this.classList.add('disabled');
-       
   };
-  
-  // buttons presses
-  letterButtons.forEach(button => {   
-   button.addEventListener('click', letterPress);
-  });
-  
-  
-  
-  
-  
+  creatWordCont();
+
+  letterButtons = document.querySelectorAll(".alphabet");
+  function checkButtons() {
+    let wrongLetter = false;
+    for (const letter of letterButtons) {
+      
+      // console.log(letter)
+      letter.addEventListener("click", function () {
+        console.log(tries); // console.log(`${letter.innerText} was clicked`)
+
+        let letterText = letter.innerText;
+        // console.log(letterText);
+        let letterels = document.querySelectorAll(".letter");
+        // console.log(letterels)
+        for (const letterel of letterels) {
+          // console.log(letterel.classList);
+          // console.log(letterText);
+          if (letterel.classList.contains(letterText.toLowerCase())) {
+            letterel.innerText = letterText;
+          } else {
+            wrongLetter = true;
+            tries -= (1 / theWord.length) ;
+            numberOfTries.textContent = Math.ceil(tries);
+          }
+        } if (Math.ceil(tries) <= 0){
+        console.log("you lost")
+          tries = 0;
+          numberOfTries.textContent = tries;
+         gameovermessage.innerText = 'You lost!'
+        }
+        console.log(tries);
+      })
+    
+    }
+  }
+  checkButtons();
+};
+
+console.log(wordContel);
+
+initiate("start");
+
+
