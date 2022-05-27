ddd68370bdf74bad3c11ae5fb6d9129c3acc8f08
andersons1218 <andersons1218@gmail.com>
2022-05-26 22:24:22 -0400

    Build-a-man winning and losing condition fixed and game completed

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 //different variables
 let selectWord;
-// console.log(selectWord);
-const winningMessage = document.querySelector('.winningmesg')
-const gameovermessage = document.querySelector('.gameover')
+const winningMessage = document.querySelector(".winningmesg");
+const gameovermessage = document.querySelector(".gameover");
 const wordContel = document.querySelector(".wordCont");
 const letterButton = document.querySelector(".letterbuttons");
 const numberOfTries = document.querySelector(".numberOfTries");
 const guesses = document.querySelector(".guesses");
-let words = ["pizza", "chips", "fries", "pancake"];
+let words = ["tacos", "chips", "fries", "nachos"];
 let tries = 5;
 let theWord;
+let currentGuess = "";
+let targetWord = "";
+let wrongLetter = null;
 
 //page refreshes picks random Word
 function randomWord() {
@@ -21,71 +23,83 @@ randomWord();
 //adding letters to HTML
 const initiate = function (game) {
   numberOfTries.textContent = tries;
-  // spaces.innerHTML = ' ';
   if (game === "start") {
     for (const i of "abcdefghijklmnopqrstuvwxyz") {
       const html = `<button class="alphabet">${i.toUpperCase()}</button>`;
       letterButton.insertAdjacentHTML("beforeend", html);
     }
   } else if (game === "reset") {
-    lettersButtons.forEach((button) => {
+    letterButton.forEach((button) => {
       button.classList.remove("disabled");
     });
   }
   selectWord = randomWord(wordContel);
   // console.log(selectWord);
 
+  //takes word and puts it in guess div
   creatWordCont = function () {
     for (let z = 0; z < selectWord.length; z++) {
       //console.log(selectWord[z])
       let letterCont = document.createElement("div");
-      letterCont.classList.add(selectWord[z]);
+      letterCont.id = selectWord[z];
       letterCont.classList.add("letter");
-      // console.log(letterCont)
       wordContel.appendChild(letterCont);
+      targetWord += selectWord[z];
     }
   };
   creatWordCont();
 
-  letterButtons = document.querySelectorAll(".alphabet");
+  // console.log(targetWord);
+
+  // Game logic
+
+  let letterButtons = document.querySelectorAll(".alphabet");
   function checkButtons() {
     let wrongLetter = false;
     for (const letter of letterButtons) {
-      
       // console.log(letter)
       letter.addEventListener("click", function () {
-        // console.log(tries); // console.log(`${letter.innerText} was clicked`)
-
+        // console.log(tries);
+        // console.log(`${letter.innerText} was clicked`)
         let letterText = letter.innerText;
+        currentGuess += letterText;
         // console.log(letterText);
         let letterels = document.querySelectorAll(".letter");
         // console.log(letterels)
-        for (const letterel of letterels) {
-          // console.log(letterel.classList);
-          // console.log(letterText);
-          if (letterel.classList.contains(letterText.toLowerCase())) {
-            letterel.innerText = letterText;
-          } else {
+        if (selectWord.includes(letterText.toLowerCase())) {
+          let letterel = document.getElementById(letterText.toLowerCase());
+          console.log(letterel);
+          letterel.innerText = letterText;
+          if (isWin(currentGuess, targetWord)) {
+              winningMessage.innerText = "You Win!!!";
+              // console.log("you win");
+            }}else {
             wrongLetter = true;
-            tries -= (1 / theWord.length) ;
+            tries -= 1 
             numberOfTries.textContent = Math.ceil(tries);
-          }
-        } if (Math.ceil(tries) <= 0){
-        // console.log("you lost")
-          tries = 0;
-          numberOfTries.textContent = tries;
-         gameovermessage.innerText = 'You lost!'
-        }
-        // console.log(tries);
-      })
-    
-    }
-  }
+            console.log(wrongLetter);
+          if (Math.ceil(tries) <= 0) {
+            // console.log("you lost")
+            tries = 0;
+            numberOfTries.textContent = tries;
+            gameovermessage.innerText = "You lost!";
+          }}
+      }
+       ) }
+      
+
+       }
   checkButtons();
 };
 
 // console.log(wordContel);
-
 initiate("start");
 
-
+// winning condition
+function isWin(guess, word) {
+  if (guess.toLowerCase() == word) {
+    return true;
+  } else {
+    return false;
+  }
+}
